a7055876e07765b6f2d2e91254029d66fdbaf724
ayushshukla3466 <ayushshukla3466@gmail.com>
2023-04-21 14:33:00 +0530

    Initial Commit

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "04e7b6e75c2ef22a5e2c479cae100d4c",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  
